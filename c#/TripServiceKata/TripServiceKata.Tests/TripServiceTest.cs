bf7206cd01fbfc8182c4cfca0b197f928e33825f
sirlexis <alex.agyapong@hotmail.com>
2018-04-19 10:57:43 +0100

    Added test for not logged user getting trips

--- c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,28 @@
-﻿namespace TripServiceKata.Tests
+﻿using NUnit.Framework;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
 {
+    [TestFixture]
     public class TripServiceTest
     {
+        [Test]
+        public void Should_not_read_trips_when_user_is_not_logged_in()
+        {
+            var tripService = new TestableTripService();
+            var user = new User.User();
+
+            Assert.That(() => tripService.GetTripsByUser(user),
+               Throws.TypeOf<UserNotLoggedInException>());
+        }
+    }
+
+    public class TestableTripService : TripService
+    {
+        protected override User.User GetLoggedUser()
+        {
+            return null;
+        }
     }
 }
